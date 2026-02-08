5778ad26d67a5b67aa2f5f19f764c86d5e831b1e
Nitish Kumar Sharma <nitishkumarsharma@Nitishs-MacBook-Air.local>
2026-02-08 16:00:44 +0530

    cors enabled

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./routing");
 
 const app = express();
